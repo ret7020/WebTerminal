b9b2ff6aa8cbacad5b12c76f9a126c8dfb629e53
ret7020 <ret7020@gmail.com>
2023-06-10 03:10:51 +0300

    Working with current working directory changing (cd command support)

--- main.py
+++ main.py
@@ -18,14 +18,33 @@ app = FastAPI(title="Web Terminal", version="0.0.1")
 templates = Jinja2Templates(directory="templates")
 
 
+class BashCd:
+    pass
+
+
 class Shell:
     def __init__(self) -> None:
         self.pids = {}
+        self.home = "/home/stephan"
+        self.path = self.home
 
     def executor(self, data):
         cmd = ["/bin/bash", "-c", data]
+
+        if "cd" in cmd[2]:
+            path = cmd[2].replace("cd", "", 1).strip()
+            if not path:  # If only cd command without any args
+                self.path = self.home
+            elif path.startswith(".."):  # TODO; go upper by tree
+                self.path = "/"
+            else:  # TODO detect absolute and relative path
+                if path[0] == "/":  # absolute path
+                    self.path = path
+                else:
+                    self.path = os.path.join(self.path, path) # Concat sub path to current path
+                    print(os.path.join(self.path, path))
         proc = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=self.path)
         self.pids[proc.pid] = proc
 
         # Send header with running process id
@@ -65,6 +84,10 @@ async def kill_process(pid: int):
 async def stream(command: CommandExecIn):
     return StreamingResponse(app.shell.executor(command.command), media_type='text/event-stream')
 
+
+@app.post("/new_exec")
+async def stream_new():
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8080, host="0.0.0.0", reload=True)
--- pty_based_client.py
+++ pty_based_client.py
@@ -5,13 +5,14 @@ output_bytes = []
 
 
 def read(fd):
+    print("Callback detectedcd")
     data = os.read(fd, 1024)
     output_bytes.append(data)
     return data
 
 
 try:
-    pty.spawn(["hashcat", "-b"], read)
+    pty.spawn(["top", "-b"], read)
     output = str(output_bytes)
 except KeyboardInterrupt:
     print(output_bytes)
